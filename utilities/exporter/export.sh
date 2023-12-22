91448a74341ce2e857ba2ec859f486bc939cf1f0
gespagne <geoffrey.espagne.pro@gmail.com>
2023-12-22 12:12:49 +0100

    Fixes

--- utilities/exporter/export.sh
+++ utilities/exporter/export.sh
@@ -26,8 +26,9 @@ log_prefix="Export"
 unzip_utility=""
 twx_url="$TWX_PROTOCOL://$TWX_HOST:$TWX_PORT/Thingworx"
 
+readonly base_remote_export_folder="src"
 readonly export_thing="Vilia.Utils.Exporter_TG"
-readonly export_folder_name="twx-src"
+readonly export_folder="export"
 readonly export_repository="SystemRepository"
 
 ###########################################################################
@@ -40,47 +41,26 @@ title() {
   echo
 }
 
-check_OS() {
-  is_windows=0
-  case "$OSTYPE" in
-    win*)     is_windows=1 ;;
-    msys*)    is_windows=1 ;;
-    cygwin*)  is_windows=1 ;;
-  esac
-}
-
 ###############################################################################
 ################################ MAIN SEQUENCE ################################
 ###############################################################################
 
-title "Will export from $twx_url"
-
-title "Step 1. Check OS"
-check_OS
-
-if [ $is_windows -eq "1" ]; then
-  echo "WINDOWS OS detected"
-  unzip_utility="util/unzip/unzip" # replace by zip xxx
-else
-  echo "UNIX-like OS detected"
-  if ! command -v unzip &> /dev/null
-  then
-      echo "unzip could not be found"
-      exit
-  fi
-  unzip_utility="unzip"
-fi
+title "Will export from :"
+twx config
 
 title "Export entities"
 
 title "Export step 1: Cleanup remote SRC folder"
-twx call "Things/$export_repository/DeleteFolder" -ppath="/${export_folder_name}"
+set +e
+twx call "Things/$export_repository/DeleteFolder" -ppath="/${base_remote_export_folder}"
+set -e
+# twx call "Things/SystemRepository/DeleteFolder" -ppath="/src"
 
 title "Export step 2: Export sources"
-twx call "Things/$export_thing/ExportSources" -prepository"${export_repository}" -ppath="${export_folder_name}"
+twx call "Things/$export_thing/ExportSources" -prepository"${export_repository}" -ppath="/${base_remote_export_folder}/${export_folder}"
 
 title "Export step 3: Zip sources"
-twx call "Things/$export_repository/CreateZipArchive" -ppath="/" -pnewFileName="twx-src.zip" -pfiles="/${export_folder_name}"
+twx call "Things/$export_repository/CreateZipArchive" -ppath="/" -pnewFileName="twx-src.zip" -pfiles="/${base_remote_export_folder}/${export_folder}"
 
 title "Export step 4: Download sources"
 twx download "$export_repository/twx-src.zip"
@@ -91,16 +71,10 @@ $unzip_utility "twx-src.zip" -d "./tmp/"
 rm twx-src.zip
 
 title "Export step 6: Cleanup local sources"
-rm -rf "$folder/twx-src"
+rm -rf "/twx-src/${export_folder}"
 
 title "Export step 7: Copy sources"
-cp -rf "tmp/${export_folder_name}" "./twx-src"
+cp -rf "tmp/${base_remote_export_folder}/${export_folder}" "./twx-src"
 rm -rf ./tmp/
-
-# TODO: remove
-if [ $is_windows -eq "1" ]; then
-  title "Export step 8: Convert line endings from UNIX to WINDOWS"
-  find "$folder" -type f -print0 |xargs -0 util/unix2dos/unix2dos -q
-fi
 
 title "Export completed successfully"
